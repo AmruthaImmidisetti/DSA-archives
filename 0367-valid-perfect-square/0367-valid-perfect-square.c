a52efe8f66ad109f52a5793d1597ce50327e666a
Amrutha Varshini Immidisetti <147792349+AmruthaImmidisetti@users.noreply.github.com>
2026-03-23 22:04:24 +0530

    Time: 0 ms (100%), Space: 8.5 MB (12.91%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.c
+++ 0367-valid-perfect-square/0367-valid-perfect-square.c
@@ -1,20 +1,6 @@
 bool isPerfectSquare(int num) 
 {
-   /* long long int t=num,flag=0;
-    for(long long int i=1;i<=num;i++)
-    {
-        if(num%i==0)
-        {
-            if((i*i)==t)   
-            {
-                flag=1;
-                break;
-            }
-        }
-    }
-    if(flag==1)    return true;
-    else           return false;*/
-    if (num <= 0) // Handling non-positive numbers
+    if (num <= 0)
         return false;
 
     int root = sqrt(num);
