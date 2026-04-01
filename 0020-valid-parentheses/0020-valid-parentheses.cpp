88a8edf1428637082bed1fe3409b5147eb83be8e
Amrutha Varshini Immidisetti <147792349+AmruthaImmidisetti@users.noreply.github.com>
2026-04-01 22:57:14 +0530

    Time: 0 ms (100%), Space: 10.1 MB (5.88%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int>st;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='('||s[i]=='['||s[i]=='{')  st.push(s[i]);
+            else if((!st.empty())&&((s[i]==')'&&st.top()=='(')||(s[i]==']'&&st.top()=='[')||(s[i]=='}'&&st.top()=='{'))) st.pop();
+            else return false;
+            if(i==s.size()-1 && !st.empty()) return false;
+        }
+        return st.empty();
+    }
+};
