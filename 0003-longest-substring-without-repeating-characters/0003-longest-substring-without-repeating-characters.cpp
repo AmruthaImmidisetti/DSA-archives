a25b6208c33ec0013e32ba8dd3cd9c3aece436ed
Amrutha Varshini Immidisetti <147792349+AmruthaImmidisetti@users.noreply.github.com>
2026-06-06 13:36:02 +0530

    Time: 13 ms (41.03%), Space: 17.8 MB (6.83%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,21 +1,33 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        map<char, int> mp;
-        int maxi = 0;
-        int i = 0, j = 0;
-        while (j < s.size()) {
-            if (!mp.count(s[j])) {
-                mp[s[j]]++;
-                j++;
-                maxi = max(maxi, j - i);
-            } else {
-                mp[s[i]]--;
-                if (mp[s[i]] == 0)
-                    mp.erase(s[i]);
-                i++;
+        if (s.empty())
+            return 0;
+        vector<int> freq(256, 0);
+        int longRepSubstr = 0;
+        string subStr = "";
+
+        for (int i = 0; i < s.size(); i++) {
+            if (freq[s[i]] == 0) {
+                freq[s[i]]++;
+                subStr += s[i];
+            } 
+            else {
+                auto it = find(subStr.begin(), subStr.end(), s[i]);
+                int ind = it - subStr.begin();
+                for (int j = 0; j <= ind; j++) {
+                    freq[subStr[j]]--;
+                }
+
+                string newSubStr(subStr.begin() + ind + 1, subStr.end());
+                subStr = newSubStr;
+                subStr += s[i];
+                freq[s[i]]++;
             }
+
+            longRepSubstr = max(longRepSubstr, (int)subStr.size());
         }
-        return maxi;
+
+        return longRepSubstr;
     }
 };
