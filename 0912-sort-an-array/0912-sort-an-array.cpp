3050fb8fb90bb9a0800dc6f61aa512a41083d89f
Amrutha Varshini Immidisetti <147792349+AmruthaImmidisetti@users.noreply.github.com>
2026-08-14 17:49:07 +0530

    Time: 664 ms (21.41%), Space: 295.8 MB (9.15%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,8 +1,41 @@
 class Solution {
 public:
-    vector<int> sortArray(vector<int>& nums) 
-    {
-         sort(nums.begin(),nums.end());
-         return nums;
+    void merge(vector<int>& a, int l, int mid, int r) {
+        vector<int> temp;
+        
+        int i = l;
+        int j = mid + 1;
+
+        while (i <= mid && j <= r) {
+            if (a[i] <= a[j])
+                temp.push_back(a[i++]);
+            else
+                temp.push_back(a[j++]);
+        }
+        while (i <= mid)
+            temp.push_back(a[i++]);
+
+        while (j <= r)
+            temp.push_back(a[j++]);
+
+        for (int k = 0; k < temp.size(); k++)
+            a[l + k] = temp[k];
+    }
+
+    void ms(vector<int>& a, int l, int r) {
+        if (l >= r)
+            return;
+
+        int mid = l + (r - l) / 2;
+
+        ms(a, l, mid);
+        ms(a, mid + 1, r);
+
+        merge(a, l, mid, r);
+    }
+
+    vector<int> sortArray(vector<int>& nums) {
+        ms(nums, 0, nums.size() - 1);
+        return nums;
     }
 };
